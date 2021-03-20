592004ebb07eb745da175044ddd19beff37514d9
Paramveer <paramveersidhu0@gmail.com>
2021-03-20 12:52:40 -0400

    Input the String

--- src/com/sidhu/Week09_Exercise.java
+++ src/com/sidhu/Week09_Exercise.java
@@ -2,6 +2,8 @@ package com.sidhu;
 
 public class Week09_Exercise {
     public static void main(String[] args) {
+        // Give a string to count each word from it.
+        String Str = "hello hello how are you HELLO are there happy test you";
 
     }
 }
