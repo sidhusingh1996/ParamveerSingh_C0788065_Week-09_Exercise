5d73b1d5e534cbd9cf46be676108cc35f980aa0e
Paramveer <paramveersidhu0@gmail.com>
2021-03-20 13:04:26 -0400

    Final Commit

--- src/com/sidhu/Week09_Exercise.java
+++ src/com/sidhu/Week09_Exercise.java
@@ -9,5 +9,22 @@ public class Week09_Exercise {
         String[] words = Str.split(" ");           // Split the word from String
         int countWord = 1;                               // Variable used to count repeated word
 
+        System.out.println("Input Str = \"" + Str + "\"");
+        System.out.println("Output: ");
+        for(int i = 0; i < words.length; i++)            // Comparison in Outer Loop
+        {
+            for(int j = i + 1; j < words.length; j++)    // Comparison in Inner Loop
+            {
+                if(words[i].equals(words[j]))            // To check whether both strings are equal or not
+                {
+                    countWord = countWord + 1;           // if equal increment the count
+                    words[j] = "0";                      // And replace repeating words with zero
+                }
+            }
+
+            if(!words[i].equals("0"))
+                System.out.println(words[i]+" -> " + countWord);    // Output of each word with count
+            countWord = 1;
+        }
     }
 }
